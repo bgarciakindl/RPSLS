ea3f48b651acd08894a47215b72d00116ff29c95
unknown <bgarciakindl@gmail.com>
2022-09-08 12:45:47 -0500

    Player Selection is enabled

--- game.py
+++ game.py
@@ -4,13 +4,16 @@ from ai import AI
 
 
 class Game:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.player_one = Human()
+        self.player_two = AI()
 
     def run_games(self):
         self.print_rules()
-        self.player_selection()
-        self.play_game()   
+        self.player_selection(self.player_one,self.player_two)
+        self.play_game() 
+        self.declare_winner()
+        self.play_again()
     
     def print_rules (self): 
         rules_of_game = ["Rock crushes Scissors","Scissors cuts Paper","Paper covers Rock","Rock crushes Lizard","Lizard poisons Spock","Spock smashes Scissors","Scissors decapitates Lizard","Lizard eats Paper","Paper disproves Spock","Spock vaporizes Rock"]
@@ -19,16 +22,13 @@ class Game:
             sleep (1)
         
     def player_selection (self,player_one,player_two):
-        player_number= input("Please Select the number of human players 1 or 2 or 3 for a surprise: ")
+        player_number= input("Please Select the number of human players 1 or 2: ")
         if player_number == 1:
-            player_one = Human("Player One")
-            player_two = AI ("Player Two")
+            player_one.name = Human("Player One")
+            player_two.name = AI ("Player Two")
         elif player_number == 2:
-           player_one = Human("Player One")
-           player_two = Human("Player Two")
-        else:
-            player_one =  AI ("Player One")
-            player_two = AI ("Player Two")
+           player_one.name = Human("Player One")
+           player_two.name = Human("Player Two")
         return player_one,player_two
     
     def play_game(self):
@@ -80,6 +80,19 @@ class Game:
                     self.player_two.score +=1
                 else:
                     print ("There is no winner of the round")
+
+    def declare_winner(self):
+        if self.player_one.score ==2:
+            print ("Player One has Won")
+        else:
+            print ("Player Two has Won")
+        
+    def play_again(self):
+        answer_equals = input("Do you wish to play again: (y/n): ") 
+        if answer_equals == "y":
+           self.run_games() 
+        else:
+            print("Thank you for playing!")
     
     #compare values and add to the score of the winner compare gesture seclection and declare a winner
     #build one game for multi player and single
